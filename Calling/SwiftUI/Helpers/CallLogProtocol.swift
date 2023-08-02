4d540ee7bff9044799ad272a4e197f7d16c78116
HassanJaved89 <hassan.javed89000@gmail.com>
2023-08-02 14:35:03 +0500

    Testing for CI/Cd testflight build.

--- Calling/SwiftUI/Helpers/CallLogProtocol.swift
+++ Calling/SwiftUI/Helpers/CallLogProtocol.swift
@@ -10,6 +10,9 @@ import Firebase
 import FirebaseFirestoreSwift
 import FirebaseStorage
 
+
+// This protocol will be used for all the Calls related stuff that includes Calling, Call logs for now. This functionality is currently implemented in ChatLogProtocol but that will be extracted because it violates the Single Responsibility principle.
+
 protocol CallLogProtocol: AnyObject, ObservableObject {
     var calls: [Call] { get set }
     var fireStoreListener: ListenerRegistration? { get set }
