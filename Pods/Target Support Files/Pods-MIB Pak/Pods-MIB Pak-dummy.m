c235bc076797afda625bad6f6e1bb64bdd7a1204
HassanJaved89 <hassan.javed89000@gmail.com>
2023-08-03 14:58:29 +0500

    Commit

---
+++ Pods/Target Support Files/Pods-MIB Pak/Pods-MIB Pak-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MIB_Pak : NSObject
+@end
+@implementation PodsDummy_Pods_MIB_Pak
+@end
---
+++ Pods/Target Support Files/Pods-MIB PakTests/Pods-MIB PakTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MIB_PakTests : NSObject
+@end
+@implementation PodsDummy_Pods_MIB_PakTests
+@end
